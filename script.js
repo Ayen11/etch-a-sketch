e417710d60ef39b19c22e00fa0e9e8943887d01e
Ayen11 <einneuman@gmail.com>
2021-12-14 18:23:02 +0200

    made the grid even and added event listener for hover, sets the color too

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const container = document.querySelector('#container');
 
-for (var i = 1; i <= 16; i++) {
+for (var i = 1; i <= 256; i++) {
     const content = document.createElement('div');
     content.classList.add('square')
-    content.textContent = i;
+    //content.textContent = i;
     container.appendChild(content);
+    content.addEventListener("mouseover", () =>{
+        content.classList.add('hovered')
+    })
 };
