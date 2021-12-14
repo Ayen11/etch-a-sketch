b7ba33f632429910af645da7784d5a1af4a17e58
Ayen11 <einneuman@gmail.com>
2021-12-14 20:39:34 +0200

    I got the grid to rescale and keep size

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+let gridsize = 64;
+function GetGridSize() {
+    gridsize = parseInt(prompt("number"));
+    console.log(gridsize);
+    setupGrid(gridsize);
+    };
+
+
+
 const container = document.querySelector('#container');
 
-for (var i = 1; i <= 256; i++) {
+for (var i = 1; i <= gridsize*gridsize; i++) {
     const content = document.createElement('div');
     content.classList.add('square')
     //content.textContent = i;
@@ -9,3 +18,26 @@ for (var i = 1; i <= 256; i++) {
         content.classList.add('hovered')
     })
 };
+
+const btn = document.querySelector('#reset');
+btn.onclick = function () {
+    GetGridSize();
+};
+
+
+
+function setupGrid(gridsize) {
+    container.style.gridTemplateColumns = `repeat(${gridsize}, 1fr)`
+    container.style.gridTemplateRows = `repeat(${gridsize}, 1fr)`
+
+for (var i = 1; i <= gridsize*gridsize; i++) {
+    const content = document.createElement('div');
+    content.classList.add('square')
+    //content.textContent = i;
+    container.appendChild(content);
+    content.addEventListener("mouseover", () =>{
+        content.classList.add('hovered')
+    })
+}};
+
+//https://www.youtube.com/watch?v=TzcmyP5MfpQ
