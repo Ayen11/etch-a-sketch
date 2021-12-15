5c528abea2779d62eee8da58211c8afe59fb2089
Ayen11 <einneuman@gmail.com>
2021-12-15 12:20:02 +0200

    add erasor button and changed text to be more user friendly

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 let gridsize = 64;
+let drawMode = 0;
+
 function GetGridSize() {
-    gridsize = parseInt(prompt("number"));
-    if (gridsize > 100) {
-        gridsize=100;
+    gridsize = parseInt(prompt("Choose grid size from 1 to 200"));
+    if (gridsize > 200) {
+        gridsize=200;
     };
     console.log(gridsize);
     setupGrid(gridsize);
     };
 
+function setDrawMode(number) {
+    drawMode = number
+};    
 
+function drawDiv(e) {
+    if (drawMode === 0) {   
+        e.target.classList.add('hovered');
+        }
+        else if (drawMode === 1){
+        e.target.classList.remove('hovered');
+        console.log('remove');
+            };
+};
 
 const container = document.querySelector('#container');
 
@@ -17,15 +31,27 @@ for (var i = 1; i <= gridsize*gridsize; i++) {
     content.classList.add('square')
     //content.textContent = i;
     container.appendChild(content);
-    content.addEventListener("mouseover", () =>{
-        content.classList.add('hovered')
-    })
+    content.addEventListener("mouseover",drawDiv);
 };
 
-const resetBtn = document.querySelector('#reset');
+
+const eraserBtn = document.querySelector('#eraser');
+eraserBtn.onclick = function () {
+    setDrawMode(1);
+    console.log('remove');
+};
+
+const penBtn = document.querySelector('#pen');
+penBtn.onclick = function () {
+    setDrawMode(0);
+};
+
+
+const resetBtn = document.querySelector('#changeSize');
 resetBtn.onclick = function () {
     GetGridSize();
 };
+
 
 
 
